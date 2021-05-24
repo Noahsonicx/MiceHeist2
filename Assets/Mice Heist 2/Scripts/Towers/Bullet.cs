4d140483340e6ac3e1ab0479e10da5d858ae3986
SteveoBurns <steveo.burns.777@gmail.com>
2021-05-25 08:46:38 +1000

    Added ..
    Lives
    Money
    Bullet

---
+++ Assets/Mice Heist 2/Scripts/Towers/Bullet.cs
@@ -0,0 +1,92 @@
+
+using UnityEngine;
+
+namespace Towers
+{
+    public class Bullet : MonoBehaviour
+    {
+        private Transform target;
+
+        public GameObject bulletEffectPrefab;
+
+        public float speed = 70f;
+        public int damage = 50;
+
+        public float explosionRadius = 0f;
+
+        public void Seek(Transform _target)
+        {
+            target = _target;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (target == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Vector3 dir = target.position - transform.position;
+            float distanceThisFrame = speed * Time.deltaTime;
+
+            if (dir.magnitude <= distanceThisFrame)
+            {
+                HitTarget();
+                return;
+            }
+
+            transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+            transform.LookAt(target);
+
+        }
+
+        void HitTarget()
+        {
+            GameObject effectIns = Instantiate(bulletEffectPrefab, transform.position, transform.rotation);
+            Destroy(effectIns, 2f);
+            if (explosionRadius > 0)
+            {
+                Explode();
+            }
+            else
+            {
+                Damage(target);
+            }
+
+
+            Destroy(gameObject);
+            Debug.Log("Hit something");
+        }
+
+        void Explode()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.tag == "Enemy")
+                {
+                    Damage(collider.transform);
+                }
+            }
+        }
+
+        void Damage(Transform enemy)
+        {
+            Enemy e = enemy.GetComponent<Enemy>();
+
+            if (e != null)
+            {
+                e.TakeDamage(damage);
+            }
+
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, explosionRadius);
+        }
+    }
+}
--- Assets/Mice Heist 2/Scripts/Towers/TowerShoot.cs
+++ Assets/Mice Heist 2/Scripts/Towers/TowerShoot.cs
@@ -7,23 +7,93 @@ namespace Towers
 
     public class TowerShoot : MonoBehaviour
     {
-        public GameObject projectile;
+        private Transform target;
 
-        // public List<Enemies> enemiesInRange = new List<Enemies>();
+        [Header("Attributes")]
+        public float range = 15f;
+        public float fireRate = 1f;
+        private float fireCountdown = 0f;
+
+        public Transform partToRatoate;
+
+        public float turnSpeed = 10f;
+
+        [Header("Unity Setup Fields")]
+        public string enemytag = "Enemy";
+
+        public GameObject bulletPrefab;
+        public Transform firePoint;
 
 
-        /*
-        if an enemy comes into range put it into a list
-
-        fire at enemies in the list
-
-        if enemy dies remove from list
-
-        if emeny gets out of range remove from list
-
-        */
 
 
-        
+        private void Start()
+        {
+            InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        }
+
+        void UpdateTarget()
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemytag);
+            float shortestDistance = Mathf.Infinity;
+            GameObject nearestEnemy = null;
+
+            foreach (GameObject enemy in enemies)
+            {
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distanceToEnemy < shortestDistance)
+                {
+                    shortestDistance = distanceToEnemy;
+                    nearestEnemy = enemy;
+                }
+            }
+
+            if (nearestEnemy != null && shortestDistance <= range)
+            {
+                target = nearestEnemy.transform;
+            }
+            else
+                target = null;
+        }
+
+
+        private void Update()
+        {
+            if (target == null)
+                return;
+            // target lock on
+            Vector3 dir = target.position - transform.position;
+            Quaternion lookRoatation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(partToRatoate.rotation, lookRoatation, Time.deltaTime * turnSpeed).eulerAngles;
+            partToRatoate.rotation = Quaternion.Euler(0, rotation.y, 0);
+
+
+            if (fireCountdown <= 0)
+            {
+                Shoot();
+                fireCountdown = 1f / fireRate;
+            }
+
+            fireCountdown -= Time.deltaTime;
+        }
+
+        void Shoot()
+        {
+
+
+            GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Bullet bullet = bulletGo.GetComponent<Bullet>();
+            if (bullet != null)
+            {
+                bullet.Seek(target);
+            }
+            Debug.Log("Shoot");
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, range);
+        }
     }
 }
