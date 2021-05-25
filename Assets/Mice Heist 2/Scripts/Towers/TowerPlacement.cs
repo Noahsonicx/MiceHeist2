01d3db3bd779871ce6417da797c599bf2985de5b
SteveoBurns <steveo.burns.777@gmail.com>
2021-05-25 18:17:27 +1000

    Build effect and cleaning up scripts

--- Assets/Mice Heist 2/Scripts/Towers/TowerPlacement.cs
+++ Assets/Mice Heist 2/Scripts/Towers/TowerPlacement.cs
@@ -10,13 +10,16 @@ namespace Towers
 
         private Tower towerToPlace;
 
-        //public GameObject buildEffect;
+        [Header("Build Effect")]
+        public GameObject buildEffect; //The effect that happens when building a tower
 
+        // Properties to call from other scripts
         public bool CanBuild { get { return towerToPlace != null; } }
         public bool HasMoney { get { return PlayerStats.Money >= towerToPlace.towerCost; } }
 
         private void Awake()
         {
+            // Creating the check for a singleton
             if (instance != null)
             {
                 Debug.LogError("More than one TowerPlacement in scene");
@@ -25,24 +28,37 @@ namespace Towers
             instance = this;
         }
 
+        /// <summary>
+        /// Sets the chosen tower as the tower to place on the node
+        /// </summary>
+        /// <param name="tower"></param>
         public void SelectTowerToPlace(Tower tower)
         {
             towerToPlace = tower;
         }
+
+        /// <summary>
+        /// Takes in the chosen node and builds the chosen tower on it
+        /// </summary>
+        /// <param name="node">Node</param>
         public void BuildTowerOn(Node node)
         {
+            // Check the player can afford the tower, if not return.
             if (PlayerStats.Money < towerToPlace.towerCost)
             {
                 Debug.Log("Not enogh money");
                 return;
             }
+            // Subtract the cost of the tower from the money variable
             PlayerStats.Money -= towerToPlace.towerCost;
 
+            // Instantiate the tower on the node and set the nodes .tower
             GameObject tower = (GameObject)Instantiate(towerToPlace.towerPrefab, node.GetBuildPosition(), Quaternion.identity);
             node.tower = tower;
 
-            //GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
-            //Destroy(effect, 5f);
+            // Instantiate the build effect and then destroy to clean up the scene
+            GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+            Destroy(effect, 5f);
 
             Debug.Log("Turret built. money left " + PlayerStats.Money);
         }
